4ccec41b6f5f545bf6990ff0cd95dfc7aebb0c0b
Josue <josh.rojas25@gmail.com>
2013-10-02 16:56:29 -0700

    Pushing recent changes

---
+++ oopintermediateprocess.php
@@ -0,0 +1,22 @@
+<?php 
+	require('oopintermediateconnection.php');
+	
+	class Process
+	{
+		var $country = "SELECT Country,Continent,Region,Population,LifeExpectancy,GovernmentForm FROM Country";
+
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
