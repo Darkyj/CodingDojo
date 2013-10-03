1962f789dfaa1990442d283c9a067fc7d1d48ddb
Josue <josh.rojas25@gmail.com>
2013-10-02 17:50:56 -0700

    Trying to incorporate OOP rather than just straight up procedural programming.

--- oopintermediate.php
+++ oopintermediate.php
@@ -1,10 +1,44 @@
+<?php 
+	require('oopintermediateconnection.php');
+	$selecting = "SELECT name FROM Country";
+	$info = new Database();
+	$info2->fetch_all($info);
+
+ ?>
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>IntermediateII OOP</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<script type="text/javascript">
+
+
+
+
+	</script>
 </head>
 <body>
-	
+	<div>
+		<h4>Select Country</h4>
+		<form action="oopintermediateprocess.php" method="post">
+			<select name="name" id="selecting">
+			<?php 
+				foreach ($info as $value) 
+				{
+					echo "<option value='{$value['name']}'>{$value['name']}</option>";
+				}
+			 ?>
+			 </select>
+			 <button>Check Info</button>
+		</form>
+	</div>
+	<div>
+		<h2>Country Information</h2>
+		<?php 
+			$information = new Process;
+			echo $information;
+		 ?>
+	</div>
 </body>
 </html>
--- oopintermediateprocess.php
+++ oopintermediateprocess.php
@@ -1,11 +1,33 @@
 <?php 
 	require('oopintermediateconnection.php');
 	
-	class Process
+	class Process extends Database
 	{
+
+		var $countryname;
+		var $cont;
+		var $region;
+		var $population;
+		var $lifeexpect;
+		var $govform;
+
 		var $country = "SELECT Country,Continent,Region,Population,LifeExpectancy,GovernmentForm FROM Country";
 
-		
+		if(isset($_POST['name'])) 
+		{
+			return var $grabbing = fetch_record($country);	
+		}
+
+		fucntion __construct()
+		{
+			$this->countryname = $grabbing['Country'];
+			$this->cont = $grabbing['Continent'];
+			$this->region = $grabbing['Region'];
+			$this->population = $grabbing['Population'];
+			$this->lifeexpect = $grabbing['LifeExpectancy'];
+			$this->govform = $grabbing['GovernmentForm'];
+		}
+
 	}
 
 
